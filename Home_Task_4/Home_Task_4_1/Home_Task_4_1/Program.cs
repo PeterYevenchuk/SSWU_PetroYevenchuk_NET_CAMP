8a6c200deaf9b5771484c90ffc3cde78b04cf882
Peter <petroyevenchuk@gmail.com>
2023-04-07 17:02:49 +0300

    Completed task 4_1
    I completed task 1. Extract data from text without concatenation

---
+++ Home_Task_4/Home_Task_4_1/Home_Task_4_1/Program.cs
@@ -0,0 +1,23 @@
+﻿using Home_Task_4_1;
+using System.Text;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Write your text!");
+        string text = Console.ReadLine();
+
+        SentencesWithParentheses sentencesWithParentheses = new SentencesWithParentheses();
+        List<string> sentencesWithInfo = sentencesWithParentheses.Sentences(text);
+
+        Console.OutputEncoding = Encoding.UTF8; // important for the proper functioning of the Ukrainian language
+
+        // Print sentences
+        Console.WriteLine("Sentences with information in brackets:");
+        foreach (string sentence in sentencesWithInfo)
+        {
+            Console.WriteLine(sentence);
+        }
+    }
+}
---
+++ Home_Task_4/Home_Task_4_1/Home_Task_4_1/SentencesWithParentheses.cs
@@ -0,0 +1,32 @@
+﻿namespace Home_Task_4_1;
+
+public class SentencesWithParentheses
+{
+    public List<string> Sentences(string text)
+    {
+        List<string> sentences = new List<string>();
+
+        int startIndex = 0;
+        for (int i = 0; i < text.Length; i++)
+        {
+            char currentChar = text[i];
+            if (currentChar == '.' || currentChar == '!' || currentChar == '?') 
+            {
+                string sentence = text.Substring(startIndex, i - startIndex + 1); // Split the text into sentences
+                sentences.Add(sentence);
+                startIndex = i + 2; // Skip the space after the period
+            }
+        }
+
+        List<string> sentencesWithInfoInBrackets = new List<string>();
+
+        foreach (string sentence in sentences)
+        {
+            if (sentence.Contains("(") && sentence.Contains(")")) // Find sentences containing information in parentheses
+            {
+                sentencesWithInfoInBrackets.Add(sentence.Trim());
+            }
+        }
+        return sentencesWithInfoInBrackets;
+    }
+}
