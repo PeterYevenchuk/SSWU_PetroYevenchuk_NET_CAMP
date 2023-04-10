f58afbabec8f8d85d5e17841e7ad1411b7faf832
Peter <petroyevenchuk@gmail.com>
2023-04-10 16:43:09 +0300

    Completed home task 4_2

---
+++ Home_Task_4/Home_Task_4_2/Home_Task_4_2/FoundEmails.cs
@@ -0,0 +1,28 @@
+﻿using System.Net.Mail;
+
+namespace Home_Task_4_2;
+
+public class FoundEmails
+{
+    public void Email(string text)
+    {
+        string[] words = text.Split(' ');
+        List<string> invalidEmails = new List<string>();
+
+        foreach (string word in words)
+        {
+            try
+            {
+                MailAddress email = new MailAddress(word);
+                Console.WriteLine("Found email: " + email.Address);
+            }
+            catch (FormatException)
+            {
+                if (word.Contains("@"))
+                {
+                    Console.WriteLine("Found invalide email: " + word);
+                }
+            }
+        }
+    }
+}
---
+++ Home_Task_4/Home_Task_4_2/Home_Task_4_2/Program.cs
@@ -0,0 +1,15 @@
+﻿using Home_Task_4_2;
+using System.Net.Mail;
+using System.Runtime.CompilerServices;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Write your text:");
+        string text = Console.ReadLine();
+
+        FoundEmails foundEmails = new FoundEmails();
+        foundEmails.Email(text);
+    }
+}
