b20f35e8c7bafd1994067f9df414aaaa0e726c9f
senivlm <91743906+senivlm@users.noreply.github.com>
2023-04-11 16:49:24 +0300

    Update Program.cs

--- Home_Task_3/Home_task_3_2/Home_Task_3_2/Program.cs
+++ Home_Task_3/Home_task_3_2/Home_Task_3_2/Program.cs
@@ -1,7 +1,7 @@
 ﻿using Home_Task_3_2.Controler;
 
 internal class Program
-{
+{//1 задача не подібно до інших, але не правильно. Можу пояснити усно...
     static void Main(string[] args)
     {
         Controler controler = new Controler();
