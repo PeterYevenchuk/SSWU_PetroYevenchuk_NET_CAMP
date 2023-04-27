1a49cfef9b04b901d579d1ba5674d4bf0493e2db
Peter <petroyevenchuk@gmail.com>
2023-04-27 19:24:18 +0300

    Completed HT 6_!

---
+++ Home_Task_6/Home_Task_6_1/Home_Task_6_1/DiagonalMatrix.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Home_Task_6_1;
+
+public class DiagonalMatrix : IEnumerable<int>
+{
+
+    private int size;
+    private int[][] matrix;
+    private int index = 0, x = -1, y = 1;
+
+    public DiagonalMatrix(int size)
+    {
+        this.size = size;
+        matrix = new int[size][];
+        for (int i = 0; i < size; i++)
+        {
+            matrix[i] = new int[size];
+        }
+
+        int o = 0;
+
+        while (o < size - 1)
+        {
+            while (y > 0 && x < size - 1)
+            {
+                y--;
+                x++;
+                AddIndexInMatrix();
+            }
+            if (y == 0 && x < size - 1)
+            {
+                x++;
+                AddIndexInMatrix();
+            }
+            else
+            {
+                y++;
+                AddIndexInMatrix();
+            }
+
+            while (x > 0 && y < size - 1)
+            {
+                y++;
+                x--;
+                AddIndexInMatrix();
+            }
+            if (x == 0 && y < size - 1)
+            {
+                y++;
+                AddIndexInMatrix();
+            }
+            else if (x == 0 && y == size - 1)
+            {
+                x++;
+                AddIndexInMatrix();
+            }
+            else
+            {
+                x++;
+                AddIndexInMatrix();
+            }
+            o++;
+        }
+    }
+
+    public void AddIndexInMatrix()
+    {
+        index++;
+        matrix[x][y] = index;
+    }
+
+    public IEnumerator<int> GetEnumerator()
+    {
+        for (int i = 0; i < size; i++)
+        {
+            for (int j = 0; j < size; j++)
+            {
+                yield return matrix[i][j];
+            }
+        }
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+}
---
+++ Home_Task_6/Home_Task_6_1/Home_Task_6_1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Home_Task_6_1;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        int sizeMatrix = 4;
+
+        DiagonalMatrix matrix = new DiagonalMatrix(sizeMatrix);
+
+        IEnumerator<int> enumerator = matrix.GetEnumerator();
+        while (enumerator.MoveNext())
+        {
+            int line = enumerator.Current;
+            Console.Write(line + " ");
+        }
+
+        /*foreach (int line in matrix)
+        {
+            Console.Write(line + " ");
+        }
+        Console.WriteLine();*/
+    }
+}
